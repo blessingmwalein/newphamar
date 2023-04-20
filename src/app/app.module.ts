d2a9a8210c209397c52b518f1f5ad4399d99dc8c
blessingmwalein <blessingmwalein@outlook.com>
2023-04-21 00:24:47 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,73 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+// layouts
+import { AdminComponent } from "./layouts/admin/admin.component";
+import { AuthComponent } from "./layouts/auth/auth.component";
+
+// admin views
+import { DashboardComponent } from "./views/admin/dashboard/dashboard.component";
+import { SettingsComponent } from "./views/admin/settings/settings.component";
+
+// auth views
+import { LoginComponent } from "./views/auth/login/login.component";
+import { RegisterComponent } from "./views/auth/register/register.component";
+
+
+
+import { AdminNavbarComponent } from "./components/navbars/admin-navbar/admin-navbar.component";
+import { AuthNavbarComponent } from "./components/navbars/auth-navbar/auth-navbar.component";
+import { FooterAdminComponent } from "./components/footers/footer-admin/footer-admin.component";
+import { FooterSmallComponent } from "./components/footers/footer-small/footer-small.component";
+import { IndexNavbarComponent } from "./components/navbars/index-navbar/index-navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthGuard } from "./services/auth/auth-guard.service";
+import { AuthInterceptorService } from "./services/auth/auth-interceptor.service";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { UsersComponent } from "./views/admin/users/users.component";
+import { RolesComponent } from "./views/admin/roles/roles.component";
+import { ChemicalsComponent } from "./views/admin/chemicals/chemicals.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserDropdownComponent,
+    SidebarComponent,
+    FooterSmallComponent,
+    FooterAdminComponent,
+    AuthNavbarComponent,
+    AdminNavbarComponent,
+    IndexNavbarComponent,
+    AdminComponent,
+    AuthComponent,
+    SettingsComponent,
+    LoginComponent,
+    RegisterComponent,
+    UsersComponent,
+    RolesComponent,
+    ChemicalsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+    AuthGuard,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/chemical.ts
@@ -0,0 +1,15 @@
+export interface Chemical {
+  name:        string;
+  description: string;
+  timeline:    string;
+  items:       Item[];
+  id:          number;
+}
+
+export interface Item {
+  name:        string;
+  description: string;
+  qty:         number;
+  chemicalId:  number;
+  id:          number | null;
+}
